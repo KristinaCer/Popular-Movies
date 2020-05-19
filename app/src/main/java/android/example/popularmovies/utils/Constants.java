61677fb872babf2cbac32191bcf72cc8423fd560
KristinaCer <k.cerniauskaite@gmail.com>
2020-05-19 20:35:14 +0200

    Removed moviesDB Api key

--- app/src/main/java/android/example/popularmovies/utils/Constants.java
+++ app/src/main/java/android/example/popularmovies/utils/Constants.java
@@ -1,7 +1,7 @@
 package android.example.popularmovies.utils;
 
 public final class Constants {
-    public static final String API_KEY = "fdc465dc2ee2fc50f353868e34491425";
+    public static final String API_KEY = "";
     public static final String BASE_URL = "https://api.themoviedb.org/3/";
     public static final String BASE_URL_F0R_IMG = "http://image.tmdb.org/t/p/w500";
     public static final String MOVIE_CATEGORY_POPULAR = "popular";
